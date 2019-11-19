e7ca6c8bb90da4b4e9f1246997011cc2f6e1fe2f
thephisics101 <thephisics101@gmail.com>
2019-11-19 11:29:28 +0200

    Switch Menu to hooks

--- src/Views/Menu.tsx
+++ src/Views/Menu.tsx
@@ -1,37 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link, Redirect} from "react-router-dom";
+
+import Modal from "../Components/Modal";
 
 import "./Menu.scss";
-import Modal from "../Components/Modal";
-import {Link} from "react-router-dom";
 
-interface State {
-  modal: boolean
-}
+export default function Menu() {
+  const [modal, setModal] = useState(false);
+  const [redirect, redirectTo] = useState<string|undefined>(undefined);
+  const [code, setCode] = useState<undefined|number>();
 
-export default class Menu extends React.Component<{}, State> {
-  state: State = {
-    modal: false
-  };
-
-  render() {
-    return <div className="menu">
-      <div className="title"><h1>Game 3</h1></div>
-      <div className="title-btns">
-        <div>
-          <button onClick={() => {this.setState({modal: true})}}>Join game</button>
-          <Link to="/create" className="btn">Create game</Link>
-          <Link to="/settings" className="btn">Settings</Link>
-        </div>
+  return <div className="menu">
+    <div className="title"><h1>Game 3</h1></div>
+    <div className="title-btns">
+      <div>
+        <button onClick={() => setModal(true)}>Join game</button>
+        <Link to="/create" className="btn">Create game</Link>
+        <Link to="/settings" className="btn">Settings</Link>
       </div>
-      <div className="footer">
-        Created by <a href="http://thephisics101.eu">ThePhisics101</a> during Nov&#8209;Dec&nbsp;2019
-      </div>
-      <Modal onClose={() => this.setState({modal: false})} open={this.state.modal}>
-        <form action={`/join/${1}`}>
-          <input type="number"/>
-          <button>Join game</button>
-        </form>
-      </Modal>
-    </div>;
-  }
+    </div>
+    <div className="footer">
+      Created by <a href="http://thephisics101.eu">ThePhisics101</a> during Nov&#8209;Dec&nbsp;2019
+    </div>
+    <Modal onClose={() => setModal(false)} open={modal}>
+      <form onSubmit={e => {
+        e.preventDefault();
+        redirectTo(`/join/${code}`)
+      }}>
+        <input type="number" value={code} onChange={({target: {value}}) => setCode(parseInt(value))}/>
+        <button>Join game</button>
+      </form>
+    </Modal>
+    {redirect ? <Redirect push to={redirect}/> : undefined}
+  </div>;
 }
